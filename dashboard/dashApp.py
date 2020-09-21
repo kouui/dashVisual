fb4aa3ff81bb99cb4566c625b8965cef007f4761
kouui <lelouchkako@gmail.com>
2020-09-22 04:32:54 +0900

    flask-dash okay

--- dashboard/dashApp.py
+++ dashboard/dashApp.py
@@ -9,251 +9,229 @@ import dash_bootstrap_components as dbc
 
 from dash.dependencies import Input, Output, State
 
-from Lib import *
+import plotly.graph_objs as go
 
+from debug import pandas_df_util as pdutil
+
+##----------------------------------------------------------------------------
+# test dataframe
+##----------------------------------------------------------------------------
 
 
 ##----------------------------------------------------------------------------
-
-data = {}
-
-
+# setup application
 ##----------------------------------------------------------------------------
 
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 #app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = html.Div([
-    html.Div([
-        html.H2("Machine Learning App"),
-        #html.Img(src="/assets/title-icon.jpg")
-    ], className="title"
-    ),
 
-    dcc.Tabs(
-        id = "tabs",
-        value = "tab-1",
-        #parent_className = "custom-tabs",
-        #className = "custom-tabs-container",
-        children = [
-            dcc.Tab(
-                label = "Configuration",
-                value = "tab-1",
-                #className = "custom-tab",
-                #selected_className = "custom-tab--selected",
-                children = [
-                    html.Br(),
+##----------------------------------------------------------------------------
+# left plot
+##----------------------------------------------------------------------------
 
-                    ##-- data path input
-                    html.Div([
-                        html.Div(
-                            dcc.Input(
-                                id = "input-dataPath",
-                                type = "text",
-                                placeholder="/path/to/read/your/datafile",
-                                className = "input-field",
-                                persistence = False,
-                                debounce=True
-                            ),style={'display': 'inline-block'},
-                        ),
-                        html.Div(
-                            html.P(
-                                id="out-input-dataPath",
-                                className = "P-output"
-                            ),style={'display': 'inline-block'}
-                        ),
-                    ], style={'width': '100%', 'display': 'inline-block'}),
+controls = dbc.Card(
+    [
+        dbc.FormGroup(
+            [
+                dbc.Label("Country"),
+                dcc.Dropdown(
+                    id="plot1-country",
+                    options=[
+                        {"label": val, "value": val} for val in pdutil._get_countries()
+                    ],
+                    value=pdutil.default_country,
+                ),
+            ]
+        ),
 
-                    html.Br(),
+        dbc.FormGroup(
+            [
+                dbc.Label("X variable"),
+                dcc.Dropdown(
+                    id="plot1-x-variable",
+                    options=[
+                        {"label": val, "value": val} for val in pdutil._get_x_variables()
+                    ],
+                    value=pdutil._get_x_variables()[0],
+                ),
+            ]
+        ),
 
-                    #-- model save input
-                    html.Div([
-                        html.Div(
-                            dcc.Input(
-                                id = "input-modelPath",
-                                type = "text",
-                                placeholder="/path/to/save/your/modelfile",
-                                className = "input-field",
-                                persistence = False,
-                                debounce=True
-                            ),style={'display': 'inline-block'},
-                        ),
-                        html.Div(
-                            html.P(
-                                id="out-input-modelPath",
-                                className = "P-output"
-                            ),style={'display': 'inline-block'}
-                        ),
-                    ], style={'width': '100%', 'display': 'inline-block'}),
+        dbc.FormGroup(
+            [
+                dbc.Label("Y variable"),
+                dcc.Dropdown(
+                    id="plot1-y-variable",
+                    options=[
+                        {"label": val, "value": val} for val in pdutil._get_y_variables()
+                    ],
+                    value=pdutil._get_y_variables()[0],
+                ),
+            ]
+        ),
 
-                    html.Br(),
+        html.Hr(),
 
-                    #-- model save input
-                    html.Div([
-                        html.Div(
-                            dcc.Input(
-                                id = "input-trainPath",
-                                type = "text",
-                                placeholder="/path/to/read/your/train/function",
-                                className = "input-field",
-                                persistence = False,
-                                debounce=True
-                            ),style={'display': 'inline-block'},
-                        ),
-                        html.Div(
-                            html.P(
-                                id="out-input-trainPath",
-                                className = "P-output"
-                            ),style={'display': 'inline-block'}
-                        ),
-                    ], style={'width': '100%', 'display': 'inline-block'}),
+        dbc.FormGroup(
+            [
+                dbc.Label("Year"),
+                dcc.Dropdown(
+                    id="plot2-year",
+                    options=[
+                        {"label": str(val), "value": int(val)} for val in pdutil._get_years()
+                    ],
+                    value=int(pdutil._get_years()[0]),
+                ),
+            ]
+        ),
 
-                    ##-- train validation split
 
-                    ##-- hyperparameter configuration
+    ],
+    body=True,
+)
 
-                    ##-- buttons
-                    html.Div([
-                        html.Div(
-                            html.Button(
-                                "Check",
-                                id='btn-check',
-                                n_clicks=0,
-                                disabled = False,
-                                className = "button-configuration"
-                            ),style={'display': 'inline-block'},
-                        ),
-                        html.Div(
-                            html.Button(
-                                "Train",
-                                id='btn-train',
-                                n_clicks=0,
-                                disabled = True,
-                                className = "button-configuration"
-                            ),style={'display': 'inline-block'},
-                        ),
-                        html.Div(
-                            html.Button(
-                                "Show Result",
-                                id='btn-show',
-                                n_clicks=0,
-                                disabled = True,
-                                className = "button-configuration"
-                            ),style={'display': 'inline-block'},
-                        ),
-                        html.Div(
-                            html.Button(
-                                "Load",
-                                id='btn-load',
-                                n_clicks=0,
-                                disabled = False,
-                                className = "button-configuration"
-                            ),style={'display': 'inline-block'},
-                        ),
-                        html.Div(
-                            html.Button(
-                                "Save",
-                                id='btn-save',
-                                n_clicks=0,
-                                disabled = True,
-                                className = "button-configuration"
-                            ),style={'display': 'inline-block'},
-                        ),
-                    ], style={'width': '100%', 'display': 'inline-block'}),
+app.layout = dbc.Container(
+    [
+        html.H1(""),
+        html.Hr(),
 
-                ]
-            ),
 
-            dcc.Tab(
-                label = "Training",
-                value = "tab-2",
-                #className = "custom-tab",
-                #selected_className = "custom-tab--selected"
-            ),
+        dbc.Col(dbc.Card([
+        dbc.Row(
+            [
+                dbc.Col(controls, md=2),
+                dbc.Col(dcc.Graph(id="plot1-graph"), md=5),
+                dbc.Col(dcc.Graph(id="plot2-graph"), md=5),
+            ],
+            align="center",
+        ),
+        ],body=True),md=12),
+    ],
+    fluid=True,
+)
 
-            dcc.Tab(
-                label = "Result",
-                value = "tab-3",
-                #className = "custom-tab",
-                #selected_className = "custom-tab--selected"
-                children = [
-                    html.Div([
-                        html.Div([
-                            html.H3('Axis'),
-                            dcc.Dropdown(
-                                id='drop-1',
-                                options=[
-                                    {'label': 'New York City', 'value': 'NYC'},
-                                    {'label': 'Montreal', 'value': 'MTL'},
-                                    {'label': 'San Francisco', 'value': 'SF'}
-                                ],
-                                #value='NYC',
-                                clearable = False,
-                                className = "dropdown-result",
-                            ),
-                            dcc.Dropdown(
-                                id='drop-2',
-                                options=[
-                                    {'label': 'New York City', 'value': 'NYC'},
-                                    {'label': 'Montreal', 'value': 'MTL'},
-                                    {'label': 'San Francisco', 'value': 'SF'}
-                                ],
-                                #value='NYC',
-                                clearable = False,
-                                className = "dropdown-result",
-                            ),
-                            ],
-                            className="result-column-1",
-                            style={'display': 'inline-block'}
-                        ),
+##----------------------------------------------------------------------------
+# make figure
+##----------------------------------------------------------------------------
+@app.callback(
+    Output("plot1-graph", "figure"),
+    [
+        Input("plot1-x-variable", "value"),
+        Input("plot1-y-variable", "value"),
+        Input("plot1-country", "value"),
+    ],
+)
+def _make_plot1(_x, _y, _country):
+    _df1 = pdutil._get_sub_df(_value=_country)
 
-                        html.Div([
-                            html.H3('Result'),
-                            dcc.Graph(
-                                id = "3dplot",
-                            ),
-                            ],
-                            className="result-column-2",
-                            style={'display': 'inline-block'}
-                        ),
+    _data = [
+        go.Scatter(
+            x=_df1[_x],
+            y=_df1[_y],
+            mode="markers+lines" if _x=="year" else "markers",
+            marker={"size": 12},
+            #name="Cluster {}".format(c),
+        ),
+    ]
 
-                        ],style={'width': '100%', 'display': 'inline-block'},
-                    ),
-                ],
-            ),
-        ],
-        vertical = False,
-    ),
+    _layout = {
+        "xaxis": {
+            "title": {
+                'text': _x,
+                "font" : {"size": 14},
+                "standoff" : 5,
+            },
+            "autorange" : True,
+            "showgrid": True,
+            "zeroline": False,
+            "showline": True,
+            "ticks": 'inside',
+            "showticklabels": True,
+        },
+        "yaxis": {
+            "title": {
+                'text': _y,
+                "font" : {"size": 14},
+                "standoff" : 5,
+            },
+            "autorange" : True,
+            "showgrid": True,
+            "zeroline": False,
+            "showline": True,
+            "ticks": 'inside',
+            "showticklabels": True,
+        },
+        "title" : {
+            'text': f"GDP per cap according to Country {_country}",
+            'y':0.95,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top',
+            "font" : {"size": 14},
+        },
+        "margin" : {
+            "l" : 5,
+            "r" : 5,
+            "b" : 5,
+            "t" : 50,
+            "pad" : 5,
+        },
+    }
 
-    #html.Div(id="tabs-content")
-])
+    return go.Figure(data=_data, layout=_layout)
 
-@app.callback(Output("out-input-dataPath", "children"), [Input("input-dataPath", "value")])
-def output_text_data_path(value):
-    if value is None:
-        value = ""
-    data["data-path"] = value
-    return "Data Path : " + value
+@app.callback(
+    Output("plot2-graph", "figure"),
+    [
+        Input("plot2-year", "value"),
+    ],
+)
+def _make_plot2(_year):
 
-@app.callback(Output("out-input-modelPath", "children"), [Input("input-modelPath", "value")])
-def output_text_model_path(value):
-    if value is None:
-        value = ""
-    data["model-path"] = value
-    return "Model Path : " + value
+    _labels, _values = pdutil._get_pie_data(_year)
 
-@app.callback(Output("out-input-trainPath", "children"), [Input("input-trainPath", "value")])
-def output_text_train_path(value):
-    if value is None:
-        return "train function from : "
-    else:
-        directory_, module_ = get_import_info_from_file_path(value)
-        try:
-            data["train"] = import_train_func_from_file_path(directory_, module_)
-            return "train function from : " + value
-        except ImportError as e :
-            return f"Error: {e}"
+    _data = [
+        go.Pie(labels=_labels, values=_values,
+               hoverinfo='label+percent', hole=.3),
+    ]
+
+    _layout = {
+        "margin" : {
+            "l" : 5,
+            "r" : 5,
+            "b" : 5,
+            "t" : 5,
+        },
+        "autosize" : False,
+    }
+
+    return go.Figure(data=_data, layout=_layout)
+
+
+##----------------------------------------------------------------------------
+# disable duplicated option in dropdown menu
+##----------------------------------------------------------------------------
+# make sure that x and y values can't be the same variable
+def _filter_options(_v, _options):
+    """Disable option v"""
+    return [
+        {"label": _val, "value": _val, "disabled": _val == _v}
+        for _val in [_item["label"] for _item in _options]
+    ]
+
+
+# functionality is the same for both dropdowns, so we reuse filter_options
+app.callback(Output("plot1-x-variable", "options"),
+             [Input("plot1-y-variable", "value")],
+             [State("plot1-x-variable", "options")])( _filter_options )
+app.callback(Output("plot1-y-variable", "options"),
+             [Input("plot1-x-variable", "value")],
+             [State("plot1-y-variable", "options")])( _filter_options )
+
+
+
 
 if __name__ == "__main__":
     app.run_server("0.0.0.0", port=8050, debug=True)
---
+++ dashboard/debug/pandas_df_util.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+df = pd.read_csv("./data/gapminderDataFiveYear.csv")
+default_country = 'Afghanistan'
+
+def _get_sub_df(_key="country", _value=default_country, _sort_key="year", _as=True):
+
+    _df1 = df[df[_key]==_value]
+    if _sort_key is not None:
+        _df1 = _df1.sort_values(by=_sort_key, ascending=_as)
+
+    return _df1
+
+def _get_countries():
+    return list( df["country"].unique() )
+
+def _get_years():
+    return sorted( list( df["year"].unique() ) )
+
+def _get_x_variables():
+    return ["year", "pop", "lifeExp", "gdpPercap"]
+
+def _get_y_variables():
+    return ["gdpPercap", "pop", "lifeExp"]
+
+def _get_pie_data(_year):
+    _df1 = _get_sub_df(_key="year", _value=_year, _sort_key="gdpPercap", _as=False)
+    _k = 30
+    _labels = _df1["country"][:_k].tolist() + ["Others"]
+    _values = _df1["gdpPercap"][:_k].tolist() +  [_df1["gdpPercap"][_k:].sum()]
+    _values = [int(_v) for _v in _values]
+    return _labels, _values
+
+
+
+
+if __name__ == "__main__":
+    # debug this script at dashVisual/dashboard/
+    df1 = _get_sub_df()
+    country_list = _get_countries()
+    print(_get_pie_data(1962))
